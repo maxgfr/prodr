cd00b516ffe440f40af19decc406b2fae7375c37
@maxgfr <maxgolfier@gmail.com>
2019-11-27 16:05:19 +0100

    push

--- app/src/main/java/com/example/prodr/activity/MainActivity.java
+++ app/src/main/java/com/example/prodr/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.prodr.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.example.prodr.R;
@@ -12,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 }
