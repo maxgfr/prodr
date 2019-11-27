1cd58a1350df765a3b6f131953241991b8121bb4
@maxgfr <maxgolfier@gmail.com>
2019-11-27 15:07:24 +0100

    push

--- app/src/main/java/com/example/prodr/activity/LoginActivity.java
+++ app/src/main/java/com/example/prodr/activity/LoginActivity.java
@@ -1,14 +1,16 @@
-package com.example.prodr;
+package com.example.prodr.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.prodr.R;
+
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
     }
 }
